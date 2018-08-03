d845d1d22b576e2391b5771828864a79b475c3e7
Quincy Hammon <qlhammon@gmail.com>
2018-08-02 19:30:31 -0500

    1st race

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,7 @@
+'use strict';
+const printListOfOlympians=(namesArray)=>{
+  namesArray.forEach(function(element){
+    console.log(element);
+  });
+}
+printListOfOlympians(['Quincy','Justin', 'Sarah', 'Michael', 'Usain']);
